dd540592f7b67991dda1d9cd7027441cbf8eb3ef
向超群 <xiangchaoqun@htmap.com>
2020-04-24 17:32:13 +0800

    1

--- 01.项目实战/手写vue框架/vue/src/笔记.js
+++ 01.项目实战/手写vue框架/vue/src/笔记.js
@@ -4,3 +4,14 @@
  * 热加载： webpack --watch
  * 打包： node_modules/.bin/webpack
  */
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    static showName() {
+        console.log(`this.name`);
+    }
+}
+Person.showName()
