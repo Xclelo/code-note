9628f1d1d6a242e1b801216944ca60c689512148
xiangchaoqun <xiangchaoqun@htmap.com>
2020-04-24 20:13:39 +0800

    性能

---
+++ 代码模板库/前端性能优化/笔记/笔记.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ * 开发 -> webserver -> 运行
+ * 开发 -> CDN -> 运行
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+ var fs = require('fs');
+ var path = require('path')
+
+ console.log(path.extname('doc/a.js'))
