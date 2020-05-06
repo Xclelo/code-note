27928e2a5891f55816f029198d45bfc38af341d4
xiangchaoqun <xiangchaoqun@htmap.com>
2020-05-06 19:03:43 +0800

    flutter笔记

--- flutter/笔记.js
+++ flutter/笔记.js
@@ -1,4 +1,11 @@
 /**
+ * 环境配置:
+ * https://gekorm.com/dart-windows/
+ * https://flutter.dev/docs/development/tools/sdk/releases
+ * https://developer.android.google.cn/studio/
+ * 
+ * 
+ * 
  * 夜神安卓模拟器 链接vscode  切换到夜神安装目录bin文件夹下:运行命令 nox_adb.exe connect 127.0.0.1:62001
  * 
  * flutter run 
@@ -16,4 +23,14 @@
  * 
  * 
  * 
+ */
+
+ /*
+ 组件笔记：
+
+ * 按钮组件： RaisedButton
+ * 基础路由： Navigator.push    Navigator.pop
+ * 命名路由： 定义：MaterialApp(routes:{'/':(context)=>Home)  使用：Navigator.pushNamed(context, '/search');
+            https://flutter.dev/docs/cookbook/navigation/navigate-with-arguments
+ 
  */
